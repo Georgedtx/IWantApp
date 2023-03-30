3ba7046578355f2fb60d84bdda051874b6b1bd07
George Dantas <56283769+Georgedtx@users.noreply.github.com>
2023-03-30 20:41:55 -0300

    Update

--- src/Domain/Entity.cs
+++ src/Domain/Entity.cs
@@ -9,7 +9,7 @@ public abstract class Entity
 
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public string CreateBy { get; set; }
+    public string CreatedBy { get; set; }
     public DateTime CreatedOn { get; set; }
     public string EditedBy { get; set; }
     public DateTime EditedOn { get; set; }
--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,14 @@
 using IWantApp.Endpoints.Categories;
+using IWantApp.Infra.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 if (app.Environment.IsDevelopment())
 {
@@ -16,5 +19,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapMethods(CategoryPost.Template,CategoryPost.Methods, CategoryPost.Handle);
+app.MapMethods(CategoryGetAll.Template,CategoryGetAll.Methods, CategoryGetAll.Handle);
+app.MapMethods(CategoryPut.Template,CategoryPut.Methods, CategoryPut.Handle);
 
 app.Run();
